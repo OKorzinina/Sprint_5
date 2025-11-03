1576c9cf2bb1d70f01b2dfe440f51e0984f50e32
OKorzinina <107882650+OKorzinina@users.noreply.github.com>
2025-11-03 21:21:30 +0100

    Add Person and RandomData classes with attributes

--- data.py
+++ data.py
@@ -1,1 +1,11 @@
+from random import randint
 
+class Person:
+    user_name = 'Olga'
+    email = f'korzininao@gmail.com'
+    password = f'12345Kor@'
+
+class RandomData:
+    user_name = 'Тест'
+    email = f'test{randint(0, 999)}@gmail.com'
+    password = f'{randint(1000, 9999)}Kor@'

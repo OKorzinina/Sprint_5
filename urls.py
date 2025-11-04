4a4d01ade42c2f27888deae8a1469ab0bbca2f20
OKorzinina <107882650+OKorzinina@users.noreply.github.com>
2025-11-04 20:46:51 +0100

    Add comments to URL constants in urls.py
    Added comments in Russian for better clarity.

--- urls.py
+++ urls.py
@@ -1,6 +1,11 @@
 class URLS:
-    MAIN_PAGE_URL = 'https://stellarburgers.nomoreparties.site/' #URL главной страницы
-    AUTH_PAGE_URL = 'https://stellarburgers.nomoreparties.site/login' #URL страницы авторизации
-    REG_PAGE_URL = 'https://stellarburgers.nomoreparties.site/register' #URL страницы регистрации
-    RECOVER_PAGE_URL = 'https://stellarburgers.nomoreparties.site/forgot-password' #URL страницы восстановления пароля
-    PROFILE_PAGE_URL = 'https://stellarburgers.nomoreparties.site/account/profile' #URL страницы личного кабинета
+    #Главная страница
+    MAIN_PAGE_URL = 'https://stellarburgers.nomoreparties.site/' 
+    #Страница авторизации
+    AUTH_PAGE_URL = 'https://stellarburgers.nomoreparties.site/login' 
+    #Страница регистрации
+    REG_PAGE_URL = 'https://stellarburgers.nomoreparties.site/register' 
+    #Страница восстановления пароля
+    RECOVER_PAGE_URL = 'https://stellarburgers.nomoreparties.site/forgot-password' 
+    #Страница ЛК
+    PROFILE_PAGE_URL = 'https://stellarburgers.nomoreparties.site/account/profile' 

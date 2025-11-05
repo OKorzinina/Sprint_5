0d2bd1d3ecf3c23c0d5994a3c7b280315f58447e
OKorzinina <107882650+OKorzinina@users.noreply.github.com>
2025-11-05 10:29:31 +0100

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,11 +1,16 @@
 class URLS:
     #Главная страница
-    MAIN_PAGE_URL = 'https://stellarburgers.nomoreparties.site/' 
+    MAIN_PAGE_URL = 'https://stellarburgers.education-services.ru/' 
+        
     #Страница авторизации
-    AUTH_PAGE_URL = 'https://stellarburgers.nomoreparties.site/login' 
+    AUTH_PAGE_URL = 'https://stellarburgers.education-services.ru/login'
+   
     #Страница регистрации
-    REG_PAGE_URL = 'https://stellarburgers.nomoreparties.site/register' 
+    REG_PAGE_URL = 'https://stellarburgers.education-services.ru/register'
+        
     #Страница восстановления пароля
-    RECOVER_PAGE_URL = 'https://stellarburgers.nomoreparties.site/forgot-password' 
+    RECOVER_PAGE_URL = 'https://stellarburgers.education-services.ru/forgot-password' 
+        
     #Страница ЛК
-    PROFILE_PAGE_URL = 'https://stellarburgers.nomoreparties.site/account/profile' 
+    PROFILE_PAGE_URL = 'https://stellarburgers.education-services.ru/account/profile' 
+        

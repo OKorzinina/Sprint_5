525c2fcb7e6264fa848e6435da0b5bfb72cd8eb1
OKorzinina <107882650+OKorzinina@users.noreply.github.com>
2025-11-05 09:06:05 +0100

    Corrected comments

--- Tests/test_profile_page.py
+++ Tests/test_profile_page.py
@@ -6,7 +6,7 @@ from selenium.webdriver.support.wait import WebDriverWait
 class TestProfileNavigation:
 
     def test_profile_button_redirects_to_profile_page(self, driver, get_login_driver):
-        """Verify successful redirection to the profile page upon clicking 'Profile' button on main page."""
+        """Тест перехода с главной страницы в ЛК через кнопку Личный кабинет"""
         browser_instance = get_login_driver
         wait = WebDriverWait(browser_instance, 10)
         wait.until(EC.visibility_of_element_located(MainPageLocators.personal_account_btn))
@@ -16,22 +16,8 @@ class TestProfileNavigation:
 
         assert browser_instance.current_url == URLS.PROFILE_PAGE_URL and is_save_button_present
 
-
-    def test_constructor_button_navigates_to_main_page_from_profile(self, driver, get_login_driver):
-        """Ensure navigation from personal profile to ingredient constructor page using the 'Constructor' button."""
-        browser_instance = get_login_driver
-        wait = WebDriverWait(browser_instance, 10)
-        wait.until(EC.visibility_of_element_located(MainPageLocators.personal_account_btn))
-        browser_instance.find_element(*MainPageLocators.personal_account_btn).click()
-        wait.until(EC.visibility_of_element_located(PersonalAreaLocators.exit_btn))
-        browser_instance.find_element(*PersonalAreaLocators.constructor_btn).click()
-        wait.until(EC.visibility_of_element_located(MainPageLocators.bun))
-        is_bun_visible = browser_instance.find_element(*MainPageLocators.bun).is_displayed()
-
-        assert browser_instance.current_url == URLS.MAIN_PAGE_URL and is_bun_visible
-
     def test_logo_click_redirects_to_constructor_page(self, driver, get_login_driver):
-        """Validate return to the ingredient constructor after clicking the logo while in personal profile."""
+        """Тест перехода из ЛК в конструктор по клику Логотип"""
         browser_instance = get_login_driver
         wait = WebDriverWait(browser_instance, 10)
         wait.until(EC.visibility_of_element_located(MainPageLocators.personal_account_btn))
@@ -43,8 +29,23 @@ class TestProfileNavigation:
 
         assert browser_instance.current_url == URLS.MAIN_PAGE_URL and is_bun_displayed
 
+    
+    def test_constructor_button_navigates_to_main_page_from_profile(self, driver, get_login_driver):
+        """Тест перехода из ЛК в конструктор по кнопке Конструктор"""
+        browser_instance = get_login_driver
+        wait = WebDriverWait(browser_instance, 10)
+        wait.until(EC.visibility_of_element_located(MainPageLocators.personal_account_btn))
+        browser_instance.find_element(*MainPageLocators.personal_account_btn).click()
+        wait.until(EC.visibility_of_element_located(PersonalAreaLocators.exit_btn))
+        browser_instance.find_element(*PersonalAreaLocators.constructor_btn).click()
+        wait.until(EC.visibility_of_element_located(MainPageLocators.bun))
+        is_bun_visible = browser_instance.find_element(*MainPageLocators.bun).is_displayed()
+
+        assert browser_instance.current_url == URLS.MAIN_PAGE_URL and is_bun_visible
+
+    
     def test_logout_button_redirects_to_login_page(self, driver, get_login_driver):
-        """Confirm redirection to login page after logging out from personal profile using the 'Logout' button."""
+        """Тест на выход из ЛК"""
         browser_instance = get_login_driver
         wait = WebDriverWait(browser_instance, 10)
         wait.until(EC.visibility_of_element_located(MainPageLocators.personal_account_btn))
